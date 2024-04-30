806dc2ea0c14c98f5ac85ffc49306716fd5e322d
fmoraless <franticzero@gmail.com>
2024-04-30 01:10:54 -0400

    feat(benefits): get benefits with data sheet

--- app/Http/Controllers/BenefitController.php
+++ app/Http/Controllers/BenefitController.php
@@ -19,16 +19,25 @@ class BenefitController extends Controller
         $filtersResponse = Http::get('https://run.mocky.io/v3/06b8dd68-7d6d-4857-85ff-b58e204acbf4');
         $filtersData = $filtersResponse->json();
 
+        $dataSheet = Http::get('https://run.mocky.io/v3/c7a4777f-e383-4122-8a89-70f29a6830c0');
+        $dataSheetData = $dataSheet->json();
+        //
+
         $filters = collect($filtersData['data'])->keyBy('id_programa');
-        //dd($filters); // 147 - 146 - 130
+        $dataSheets = collect($dataSheetData['data'])->keyBy('id');
 
         $benefitsByYear = collect($benefitsData['data'])->groupBy(function ($item) {
             return substr($item['fecha'], 0, 4);
-        })->map(function ($benefits, $year) use ($filters) {
+        })->map(function ($benefits, $year) use ($filters, $dataSheets) {
             $filteredBenefits = $benefits->filter(function ($benefit) use ($filters) {
                 $filter = $filters->get($benefit['id_programa']);
 
                 return $benefit['monto'] >= $filter['min'] && $benefit['monto'] <= $filter['max'];
+            })->map(function ($benefit) use ($filters, $dataSheets) {
+                $filter = $filters->get($benefit['id_programa']);
+                $ficha = $dataSheets->get($filter['ficha_id']);
+
+                return array_merge($benefit, ['ficha' => $ficha]);
             });
             $totalByYear = $filteredBenefits->sum('monto');
 
